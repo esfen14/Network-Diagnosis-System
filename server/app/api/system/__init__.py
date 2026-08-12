ac90fc3ef75a2d74b352f476fcc203b23ccca725
esfen <167210728+esfen14@users.noreply.github.com>
2026-08-12 17:28:33 +0800

    fixed the from in the system __init__.py for log

--- server/app/api/system/__init__.py
+++ server/app/api/system/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 system_bp = Blueprint('system', __name__, url_prefix='/system')
 
 from app.api.system import network_discovery
-from app.api.system import logs
+from app.api.system import log
 from app.api.system import report
 from app.api.system import settings
