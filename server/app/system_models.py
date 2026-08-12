4aa85d12e1d9f7128e15c2c1e533c31a8dc0ce0f
esfen <167210728+esfen14@users.noreply.github.com>
2026-08-12 18:49:43 +0800

    removed Closed_At column for TCP and UCP services tables

--- server/app/system_models.py
+++ server/app/system_models.py
@@ -328,7 +328,7 @@ class Open_TCP_Services(db.Model):
     OpenPortID: so.Mapped[int] = so.mapped_column(primary_key=True)
     Port_Number: so.Mapped[int] = so.mapped_column()
     Serivce_Name: so.Mapped[str] = so.mapped_column(sa.String(255))
-    Closed_At: so.Mapped[Optional[datetime]] = so.mapped_column()
+    # Closed_At: so.Mapped[Optional[datetime]] = so.mapped_column()
 
     NetDiscoveryID: so.Mapped[int] = so.mapped_column(sa.ForeignKey(NetworkDiscovery.NetDiscoveryID), index=True)
 
@@ -338,7 +338,7 @@ class Open_UDP_Services(db.Model):
     OpenPortID: so.Mapped[int] = so.mapped_column(primary_key=True)
     Port_Number: so.Mapped[int] = so.mapped_column()
     Service_Name: so.Mapped[str] = so.mapped_column(sa.String(255))
-    Closed_At: so.Mapped[Optional[datetime]] = so.mapped_column()
+    # Closed_At: so.Mapped[Optional[datetime]] = so.mapped_column()
     
     NetDiscoveryID: so.Mapped[int] = so.mapped_column(sa.ForeignKey(NetworkDiscovery.NetDiscoveryID), index=True)
 
