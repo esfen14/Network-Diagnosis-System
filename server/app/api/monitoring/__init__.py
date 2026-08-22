5a5427d0a229e061875e94d568f1af7a74f5d790
esfen <167210728+esfen14@users.noreply.github.com>
2026-08-22 11:39:29 +0800

    removed dashboard and network health from api, replaced with statistics in system api

--- server/app/api/monitoring/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-monitoring_bp = Blueprint('monitoring', __name__, url_prefix='/monitoring')
-
-from app.api.monitoring import dashboard
-from app.api.monitoring import network_health
